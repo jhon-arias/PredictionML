17d21db7d967d665af4cec6ad22bbf5b32e92470
jhon-arias <jwariasch@gmail.com>
2023-10-18 23:57:37 -0500

    Filtramos los casos de hidrocarburos

--- PredictionML.R
+++ PredictionML.R
@@ -9,23 +9,32 @@ load("Acciones.Rdata")
 #Los años con mayor cantidad de datos (94.3%) son 2019-2023 (2,353 emergencias) 
 #43 emergencias no tienen fecha de emergencia
 #Filtramos los que nose conoce el administrados y los que cuentan con unidad fiscalizable conocida
+#Filtramos solo hidrocarburos ya que es la mayor casuistica
 
-Emergencias_R %>% 
-  filter(!is.na(FE_FECHA_EMERG), FE_FECHA_EMERG>=as.Date("2019-01-01")) %>%
-  filter(!str_detect(TX_ADMINISTRADO, "NO DETERMINADO"), !is.na(ID_UF_CSIG)) %>%
+EmergenciasHID<- Emergencias_R %>%
+  filter(!is.na(FE_FECHA_EMERG), 
+         FE_FECHA_EMERG>=as.Date("2019-01-01"),
+         #FE_FECHA_EMERG>=as.Date("2019-12-31"),
+         !str_detect(TX_ADMINISTRADO, "NO DETERMINADO"), 
+         !is.na(ID_UF_CSIG),
+         str_detect(TX_SUB_SECTOR, "Hidrocarburos")) %>%
   select(-c(TX_DEPARTAMENTO, TX_PROVINCIA, TX_DISTRITO)) %>%
-  distinct() %>%
-  group_by(ID_UF_CSIG, ANHO = year(FE_FECHA_EMERG)) %>% summarise(codigos = n_distinct(TX_CODIGO, na.rm = TRUE)) %>% 
-  spread(ANHO,codigos) %>% janitor::adorn_totals(c("row", "col"))->TABLAS
+  distinct() 
+# %>% view()
+#   group_by(TX_SUB_SECTOR, ANHO = year(FE_FECHA_EMERG)) %>% summarise(codigos = n_distinct(TX_CODIGO, na.rm = TRUE), UF = n_distinct(ID_UF_CSIG)) %>% 
+#   spread(ANHO,codigos) %>% janitor::adorn_totals(c("row", "col"))
 
 
-
-
+EmergenciasUF<- as.vector(unique(EmergenciasHID$ID_UF_CSIG))
 
 #Revisamos las supervisiones realizadas
 
-Acciones %>% filter(FEFIN>=as.Date("2018-01-01"),str_detect(IDUF_SIG,"UF0002772"), !str_detect(TXESTADO,"ANULADA") ) %>%
-  view()
+Acciones %>% 
+  filter( str_detect(TXCOORDINACION,"HIDROCARBUROS") ,
+          !str_detect(TXESTADO,"ANULADA|PLANIFICACIÓN|PROGRAMADA|NO EJECUTADA|EN REVISION"),
+          FEFIN>= as.Date("2018-01-01"),
+          IDUF_SIG %in% EmergenciasUF) %>% view()
+
 
 
 
